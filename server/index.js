982a5cabf13f6ce73798948fd2dc9a4ded2ed75b
YummySalamy <84691405+YummySalamy@users.noreply.github.com>
2023-05-16 22:57:29 -0500

    Update index.js
    In this commit, I made several changes to enhance the codebase's overall quality and facilitate collaboration with other project contributors. The main focus was on improving code readability and adhering to best programming practices, while also ensuring the code is as scalable as possible.
    
    I refactored the existing code by reorganizing and restructuring it in a more logical and coherent manner. This involved renaming variables and functions to be more descriptive, improving the consistency of naming conventions, and removing any redundant or unnecessary code.

--- server/index.js
+++ server/index.js
@@ -1,88 +1,66 @@
-// SERVER INITIALIZATION
+// WE DON't EVEN KNOW WHAT WE DOIN BUT WE UPPPPPPP!!!!!!!(insert'fire'emoji')
 const express = require('express');
 const app = express();
 const path = require('path');
 const http = require('http');
+const { SerialPort } = require('serialport');
+const { DelimiterParser } = require('@serialport/parser-delimiter');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-// STATIC ROOTS
-const STATIC_ROOT = '../client/template';
-const INDEX_ROOT = STATIC_ROOT + '/index.html';
-const FIELD_VS_DISTANCE_ROOT = STATIC_ROOT + '/field-vs-distance.html';
-const MAGNETIC_FLUX_ROOT = STATIC_ROOT + '/magnetic-flux.html';
+const STATIC_ROOT = path.join(__dirname, '../client/template');
+const INDEX_ROOT = path.join(STATIC_ROOT, 'index.html');
+const FIELD_VS_DISTANCE_ROOT = path.join(STATIC_ROOT, 'field-vs-distance.html');
+const MAGNETIC_FLUX_ROOT = path.join(STATIC_ROOT, 'magnetic-flux.html');
 
-var serialConnected = true;
-const { SerialPort } = require('serialport')
-const { DelimiterParser } = require('@serialport/parser-delimiter')
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(STATIC_ROOT));
 
-//SOCKET INITIALIZATION
-const server = require('http').createServer(app);
-const io = require('socket.io')(server)
-
-
-
-
-io.on('connection',function(socket){
-  console.log("A new socket connected");
+app.get('/', (req, res) => {
+  res.sendFile(INDEX_ROOT);
 });
 
-
-//SETTINGS
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, STATIC_ROOT)));
-
-// SERVER ROOTS
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, INDEX_ROOT));
-  });
-
-app.get('/field-vs-distance', (req, res) =>{
-    res.sendFile(path.join(__dirname, FIELD_VS_DISTANCE_ROOT))
+app.get('/field-vs-distance', (req, res) => {
+  res.sendFile(FIELD_VS_DISTANCE_ROOT);
 });
 
 app.get('/magnetic-flux', (req, res) => {
-    res.sendFile(path.join(__dirname, MAGNETIC_FLUX_ROOT))
-})
+  res.sendFile(MAGNETIC_FLUX_ROOT);
+});
 
+app.use('/socket.io', express.static(path.join(__dirname, '../node_modules/socket.io/client-dist')));
 
-app.get('/socket.io/socket.io.js', (req, res) => {
-    res.sendFile(path.join(__dirname , '../node_modules/socket.io/client-dist/socket.io.js'));
-  });
-  console.log(path.join(__dirname , '../node_modules/socket.io/client-dist/socket.io.js'));
-
-
-//Socket connection
 server.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
-})
+  console.log('Server listening on port', app.get('port'));
+});
 
+io.on('connection', (socket) => {
+  console.log('A new socket connected');
+});
 
-// Serial port initialization
-const port = new SerialPort(
-    {
-        path: 'COM3',
-        baudRate: 9600
-    }
-)
+//Serial port initialization (par el arduino xdxddx)
+const port = new SerialPort({
+  path: 'COM3',
+  baudRate: 9600
+});
 
-const parser = port.pipe(new DelimiterParser({ delimiter: '\n' }))
+const parser = port.pipe(new DelimiterParser({ delimiter: '\n' }));
 
 //Parser listener funcs
-parser.on('open',function(){
-   console.log('Serial running')
+parser.on('open', () => {
+  console.log('Serial port open');
 });
 
-parser.on('data',function(data){
-    var enc = new TextDecoder();
-    var arr = new Uint8Array(data);
-    ready = enc.decode(arr)
-    console.log(ready)
-    io.emit('arduino:data', {
-        value: ready
-    })
+parser.on('data', (data) => {
+  const enc = new TextDecoder();
+  const arr = new Uint8Array(data);
+  const ready = enc.decode(arr);
+  console.log(ready);
+  io.emit('arduino:data', {
+    value: ready
+  });
 });
 
-port.on('error',function(err){
-    console.log(err)
-})
-
-//Socket will comunicate the arduino with the web, only localhost
+port.on('error', (err) => {
+  console.log(err);
+});
